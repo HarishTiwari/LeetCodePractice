e619234cb9fa586ec5898e385641d6214b807feb
Harish Tiwari <harishtiwari1998@gmail.com>
2025-06-08 17:27:35 +0530

    File updated at 6/8/2025, 5:27:34 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,40 +1,23 @@
-// Last updated: 6/8/2025, 4:11:44 PM
+// Last updated: 6/8/2025, 5:27:34 PM
 class Solution {
-    public int subarraySum(int[] arr, int k) {
+    public int subarraySum(int[] nums, int k) {
 
-        int count = 0;
+    Map<Integer, Integer> map = new HashMap<>();
+    map.put(0, 1); 
 
-        // calculate the prefix sum of array
-        int[] prefixArr = calculatePrefixSum(arr);
+    int prefixSum = 0;
+    int count = 0;
 
-        // calculating each subarray sum
+    for (int num : nums) {
+        prefixSum += num;
 
-        for(int i=0 ; i < arr.length ; i ++){
-            for(int j = i ; j < arr.length ; j++){
-
-                int sumOfSubarr = (i !=0) ? prefixArr[j] - prefixArr[i-1] : prefixArr[j]; 
-
-                if(sumOfSubarr == k) count++;
-
-            }
+        if (map.containsKey(prefixSum - k)) {
+            count += map.get(prefixSum - k);
         }
 
-        return count;
-        
+        map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
     }
 
-    public int[] calculatePrefixSum(int[] arr){
-
-        int[] prefixArr = new int[arr.length];
-
-        prefixArr[0] = arr[0];
-
-        for(int i=1 ; i < arr.length ; i++){
-         prefixArr[i] =  prefixArr[i-1] + arr[i];  
-        }
-
-        return prefixArr;
+    return count;
     }
-
-    
 }
