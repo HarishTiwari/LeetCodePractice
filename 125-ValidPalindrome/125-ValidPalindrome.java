373324f9e966de13ef7b595cefe1bf0295dba0a5
Harish Tiwari <harishtiwari1998@gmail.com>
2025-06-29 14:50:03 +0530

    File updated at 6/29/2025, 2:50:02 PM

--- 125-ValidPalindrome/125-ValidPalindrome.java
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -1,65 +1,22 @@
-// Last updated: 5/27/2025, 12:47:00 AM
+// Last updated: 6/29/2025, 2:50:02 PM
 class Solution {
-    
-    
     public boolean isPalindrome(String s) {
-        
-        if (s == null) return false;
-        if (s.isEmpty()) return true;
-        s = s.toLowerCase();
 
-        int l = 0, r = s.length() - 1;
-        while (l < r) {
-            while (!Character.isLetterOrDigit(s.charAt(l))) {
-                l++;
-                if (l == r) return true;
-            }
-            while (!Character.isLetterOrDigit(s.charAt(r))) r--;
+        String ans = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
 
-            if (s.charAt(l) == s.charAt(r)) {
-                l++;
-                r--;
-            } else {
+        // applying two pointer approach
+
+        int start = 0;
+        int end = ans.length()-1;
+
+        while(start < end){
+            if(ans.charAt(start) == ans.charAt(end)){
+                start++;
+                end--;
+            }else{
                 return false;
             }
         }
         return true;
-        
     }
-//     public boolean isPalindrome(String s) {
-        
-//         if(s.length()==0 || s.length() == 1)
-//             return true;
-        
-//         s = s.replaceAll("\\s","");
-//         s = s.replaceAll("[^a-zA-Z0-9]", "");
-//         s = s.toLowerCase();
-//         //System.out.println(s);
-        
-//         String rev = reverseString(s);
-
-//         if (s.equals(rev)) return true;
-//         else return false;
-        
-//     }
-    
-//     private String reverseStringRecursion(String str) {
-
-//         if (str.length() == 1 || str.length() == 0) {
-//             return str;
-//         }
-
-//         return reverseStringRecursion(str.substring(1)) + "" + str.charAt(0);
-//     }
-    
-//     private String reverseString(String str) {
-
-//         String rev = "";
-
-//         for (int i = str.length() - 1; i >= 0; i--) {
-//             rev += str.charAt(i);
-//         }
-
-//         return rev;
-//     }
 }
